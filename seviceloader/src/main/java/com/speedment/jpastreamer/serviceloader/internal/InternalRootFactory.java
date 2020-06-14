fd2c1ae82d1cf72fc8a0d5ff682ad684f575e847
Per Minborg <minborg@speedment.com>
2020-06-14 13:16:44 +0200

    Add missing change in commit

--- seviceloader/src/main/java/com/speedment/jpastreamer/serviceloader/internal/InternalRootFactory.java
+++ seviceloader/src/main/java/com/speedment/jpastreamer/serviceloader/internal/InternalRootFactory.java
@@ -1,13 +1,12 @@
 package com.speedment.jpastreamer.serviceloader.internal;
 
-import java.util.Iterator;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.ServiceLoader;
+import java.rmi.server.ServerCloneException;
+import java.util.*;
 
-public final class RootFactory {
+public final class InternalRootFactory {
 
-    private RootFactory() {}
+    private InternalRootFactory() {
+    }
 
     public static <S> Optional<S> get(final Class<S> service) {
         return Optional.ofNullable(getHelper(service));
@@ -27,8 +26,40 @@ public final class RootFactory {
         if (iterator.hasNext()) {
             return iterator.next();
         } else {
-            return null;
+            // If not found though the ServiceLoader, try to guess the standard implementation
+            return getStandard(service);
         }
+    }
+
+    private static <S> S getStandard(final Class<S> service) {
+        final String standardServiceName = service.getPackage().getName() + ".standard." + "Standard" + service.getSimpleName();
+        Class<?> clazz = null;
+        try {
+            clazz = Class.forName(standardServiceName);
+        } catch (ClassNotFoundException x) {
+            fail(service, "Provider " + standardServiceName + " not found");
+        }
+        if (!service.isAssignableFrom(clazz)) {
+            fail(service, "Provider " + standardServiceName + " not a subtype");
+        }
+        try {
+            final Object instance = clazz.newInstance();
+            final S p = service.cast(instance);
+            System.out.println("Warning: " + service + " implementation guessed to be " + instance.getClass().getName() + ". " +
+                    "This should be fixed to ensure performance and stability.");
+            return p;
+        } catch (Throwable x) {
+            fail(service, "Provider " + standardServiceName + " could not be instantiated", x);
+        }
+        throw new NoSuchElementException("Error"); // we should never end up here
+    }
+
+    private static void fail(Class<?> service, String msg, Throwable cause) throws ServiceConfigurationError {
+        throw new ServiceConfigurationError(service.getName() + ": " + msg, cause);
+    }
+
+    private static void fail(Class<?> service, String msg) throws ServiceConfigurationError {
+        throw new ServiceConfigurationError(service.getName() + ": " + msg);
     }
 
 }
