39e47f45998ccaeee4095fd26f9c8e4ad5583311
Mislav Milicevic <mislav.milicevic22@gmail.com>
2020-06-16 17:10:53 +0200

    Use Renderer as a Stream provider in Streamer

--- provider/application-standard/src/main/java/com/speedment/jpastreamer/application/standard/internal/StandardStreamer.java
+++ provider/application-standard/src/main/java/com/speedment/jpastreamer/application/standard/internal/StandardStreamer.java
@@ -1,40 +1,41 @@
 package com.speedment.jpastreamer.application.standard.internal;
 
-import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.persistence.TypedQuery;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
 
+import com.speedment.jpastreamer.pipeline.Pipeline;
+import com.speedment.jpastreamer.pipeline.PipelineFactory;
+import com.speedment.jpastreamer.renderer.RenderResult;
+import com.speedment.jpastreamer.renderer.Renderer;
+import com.speedment.jpastreamer.renderer.RendererFactory;
+import com.speedment.jpastreamer.rootfactory.RootFactory;
+
 final class StandardStreamer<E> implements Streamer<E> {
 
+    private final Renderer renderer;
+    private final PipelineFactory pipelineFactory;
+
     private final Class<E> entityClass;
-    private final EntityManager entityManager;
 
     StandardStreamer(final Class<E> entityClass, final EntityManagerFactory entityManagerFactory) {
         this.entityClass = requireNonNull(entityClass);
-        this.entityManager = entityManagerFactory.createEntityManager();
+        this.pipelineFactory = RootFactory.getOrThrow(PipelineFactory.class);
+        this.renderer = RootFactory.getOrThrow(RendererFactory.class)
+            .createRenderer(entityManagerFactory);
     }
 
     @Override
     public Stream<E> stream() {
-        final CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
-        final CriteriaQuery<E> criteriaQuery = criteriaBuilder.createQuery(entityClass);
-        final Root<E> root = criteriaQuery.from(entityClass);
-        final TypedQuery<E> typedQuery = entityManager.createQuery(criteriaQuery);
-        criteriaQuery.select(root);
-        final Stream<E> stream = typedQuery.getResultStream();
-        return stream;
+        final Pipeline<E> pipeline = pipelineFactory.createPipeline(entityClass);
+
+        return renderer.render(pipeline).stream();
     }
 
     @Override
     public void close() {
         System.out.println("Closing Streamer<" + entityClass.getSimpleName() + ">");
-        entityManager.close();
+        renderer.close();
     }
-
 }
