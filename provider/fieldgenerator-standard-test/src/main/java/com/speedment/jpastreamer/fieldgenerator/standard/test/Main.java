046646da5679b8d8b44c7927b213a0f19d311c5e
Per Minborg <minborg@speedment.com>
2020-06-17 19:38:14 +0200

    Expand manual test

--- provider/fieldgenerator-standard-test/src/main/java/com/speedment/jpastreamer/fieldgenerator/standard/test/Main.java
+++ provider/fieldgenerator-standard-test/src/main/java/com/speedment/jpastreamer/fieldgenerator/standard/test/Main.java
@@ -1,8 +1,6 @@
 package com.speedment.jpastreamer.fieldgenerator.standard.test;
 
 
-import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -10,24 +8,39 @@ public class Main {
     public static void main(String[] args) {
         final Film olle = new Film();
         olle.setFilmId(1);
-        olle.setTitle("Moderns Dimunitiva Olle");
+        olle.setTitle("Moderns Dimunitiva Son Vid Namn Olle");
         olle.setDescription("Olle går i skogen och träffar en björn som äter upp alla hans surt förvärvade blåbär. Olle kräver sedan oväntat av sin mamma att få återse den fyrbenta tjuven.");
         olle.setLength(120);
+        olle.setRating("G");
+
+        System.out.println(olle);
 
         final Film spindel = new Film();
         olle.setFilmId(2);
-        olle.setTitle("Vimsiga Spindeln");
+        olle.setTitle("Den Disorienterade Spindeln");
         olle.setDescription("Spindeln klättrar upp för sin tråd men allt går ãt helvete och spindeln faller ned. Han ger dock ej upp utan försöker ånyo ad infinitum!");
-        olle.setLength(100);
+        olle.setLength(110);
+        olle.setRating("G");
+
+        System.out.println(olle);
+
+        final Film f = new Film();
+        System.out.println(f);
+
+        System.out.println("**  Streaming **");
 
 
-        List<Film> filmList = Stream.of(olle, spindel)
-                .filter(Film$.length.between(100, 150))
-                .sorted(Film$.title)
-                .collect(Collectors.toList());
+        Stream.of(olle, spindel)
+/*                .filter(Film$.length.between(100, 150))*/
+/*  .filter(Film$.rating.in("G", "PG")) */
+/*                .sorted(Film$.title)*/
+                .forEachOrdered(System.out::println);
 
 
-        System.out.println(filmList);
+        System.out.println("**  After Streaming **");
+        System.out.println(olle);
+        System.out.println(spindel);
+
     }
 
 }
