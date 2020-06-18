da460d5a2505bec351d36c33c7fb838f685b8905
Per Minborg <minborg@speedment.com>
2020-06-18 19:48:45 +0200

    Add logo at startup

--- provider/application-standard/src/main/java/com/speedment/jpastreamer/application/standard/internal/StandardJpaStreamerBuilder.java
+++ provider/application-standard/src/main/java/com/speedment/jpastreamer/application/standard/internal/StandardJpaStreamerBuilder.java
@@ -17,6 +17,21 @@ public final class StandardJpaStreamerBuilder implements JpaStreamerBuilder {
 
     @Override
     public JpaStreamer build() {
+
+        // http://patorjk.com/software/taag/#p=display&f=Doom&t=JpaStreamer
+
+        final String welcome =
+                "   ___             _____ _                                      \n" +
+                "  |_  |           /  ___| |                                     \n" +
+                "    | |_ __   __ _\\ `--.| |_ _ __ ___  __ _ _ __ ___   ___ _ __ \n" +
+                "    | | '_ \\ / _` |`--. \\ __| '__/ _ \\/ _` | '_ ` _ \\ / _ \\ '__|\n" +
+                "/\\__/ / |_) | (_| /\\__/ / |_| | |  __/ (_| | | | | | |  __/ |   \n" +
+                "\\____/| .__/ \\__,_\\____/ \\__|_|  \\___|\\__,_|_| |_| |_|\\___|_|   \n" +
+                "      | |                                                       \n" +
+                "      |_|   ";
+
+        System.out.println(welcome);
+
         return new StandardJpaStreamer(entityManagerFactory);
     }
 
