7351bd5b9c80544dba56b41318d7bcde7187e4cf
Mislav Milicevic <mislav.milicevic22@gmail.com>
2020-06-19 16:27:13 +0200

    Add Criteria modules

---
+++ criteria/src/main/java/com/speedment/jpastreamer/criteria/CriteriaFactory.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2006-2020, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.speedment.jpastreamer.criteria;
+
+import com.speedment.jpastreamer.field.predicate.SpeedmentPredicate;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+
+/**
+ * @author Mislav Milicevic
+ * @since 0.0.9
+ */
+public interface CriteriaFactory {
+
+    /**
+     * Creates and returns a JPA {@code Predicate} with the {@code SpeedmentPredicate} serving
+     * as the model. The JPA {@code Predicate} is created using the provided {@code criteriaBuilder}
+     *
+     * @param criteriaBuilder used to create the JPA Predicate
+     * @param speedmentPredicate used as a model for the JPA Predicate that is being created
+     * @param <T> root entity used in the Speedment Predicate
+     * @return JPA Predicate
+     */
+    <T> Predicate createPredicate(
+        final CriteriaBuilder criteriaBuilder,
+        final SpeedmentPredicate<T> speedmentPredicate
+    );
+}
---
+++ provider/criteria-standard/src/main/java/com/speedment/jpastreamer/criteria/standard/internal/InternalCriteriaFactory.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2006-2020, Speedment, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); You may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at:
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.speedment.jpastreamer.criteria.standard.internal;
+
+import com.speedment.jpastreamer.criteria.CriteriaFactory;
+import com.speedment.jpastreamer.field.predicate.SpeedmentPredicate;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Predicate;
+
+public final class InternalCriteriaFactory implements CriteriaFactory {
+
+    @Override
+    public <T> Predicate createPredicate(
+        final CriteriaBuilder criteriaBuilder,
+        final SpeedmentPredicate<T> speedmentPredicate
+    ) {
+        throw new UnsupportedOperationException("todo");
+    }
+}
