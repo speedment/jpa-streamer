e8953bc7864861f7f7eb4f93e27c0291cafa1ed3
Julia Gustafsson <gustafsson.julia@live.se>
2020-07-02 08:54:41 +0200

    Publish in Stream fundamentals

---
+++ docs/modules/stream-fundamentals/examples/SoccerExample.java
@@ -0,0 +1,11 @@
+Stream.of(                           // Creates a Stream with the given content
+        "Zlatan",
+        "Tim",
+        "Bo",
+        "George",
+        "Adam",
+        "Oscar"
+        )
+        .filter(n -> n.length() > 2)     // Retains only those Strings that are longer than 2 characters (i.e. "Bo" is dropped)
+        .sorted()                        // Sorts the remaining Strings in natural order
+        .collect(Collectors.toList());   // Collects the remaining sorted Strings in a List
