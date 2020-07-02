5ce1fd17959f3164a377461439437d9e43a0b924
Julia Gustafsson <gustafsson.julia@live.se>
2020-07-02 16:07:03 +0200

    docs: Add page about intermediate operations

--- docs/modules/stream-fundamentals/examples/SoccerExample.java
+++ docs/modules/stream-fundamentals/examples/SoccerExample.java
@@ -6,6 +6,9 @@ Stream.of(                           // Creates a Stream with the given content
         "Adam",
         "Oscar"
         )
-        .filter(n -> n.length() > 2)     // Retains only those Strings that are longer than 2 characters (i.e. "Bo" is dropped)
-        .sorted()                        // Sorts the remaining Strings in natural order
-        .collect(Collectors.toList());   // Collects the remaining sorted Strings in a List
+        .filter(n -> n.length() > 2)
+        // Retains only those Strings that are longer than 2 characters (i.e. "Bo" is dropped)
+        .sorted()
+        // Sorts the remaining Strings in natural order
+        .collect(Collectors.toList());
+        // Collects the remaining sorted Strings in a List
