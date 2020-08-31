2e1b6aa2e9c2ccabcee36513229bcb0ed3216c35
Per Minborg <minborg@speedment.com>
2020-08-31 17:24:26 +0200

    Document the JPAStreamer class, Fix #50

--- application/src/main/java/com/speedment/jpastreamer/application/JPAStreamer.java
+++ application/src/main/java/com/speedment/jpastreamer/application/JPAStreamer.java
@@ -18,17 +18,168 @@ import javax.persistence.EntityManagerFactory;
 import java.util.ServiceLoader;
 import java.util.stream.Stream;
 
+/**
+ * A JPAStreamer is responsible for creating Streams from data sources
+ * Entity sources can be RDBMSes, files or other data sources.
+ *
+ * A JPAStreamer must be thread safe and be able to handle several reading and
+ * writing threads at the same time.
+ *
+ * @author Per Minborg
+ * @since 0.1.0
+ */
 public interface JPAStreamer {
 
+    /**
+     * Creates and returns a new {@link Stream} over all entities in the
+     * underlying data source (e.g database) of the provided type {@code entityClass}.
+     * This is the main query API for JPAstreamer.
+     * <p>
+     * The order in which elements are returned when the stream is eventually
+     * consumed <em>is unspecified</em>. The order may even change from one
+     * invocation to another. Thus, it is an error to assume any particular
+     * element order even though is might appear, for some stream sources, that
+     * there is a de-facto order.
+     * <p>
+     * If a deterministic order is required, then make sure to invoke the
+     * {@link Stream#sorted(java.util.Comparator)} method on the {@link Stream}
+     * returned.
+     * <p>
+     * Mutable elements are not reused within the stream. More formally, there
+     * are no pair of mutable stream elements <code>e1</code> and
+     * <code>e2</code> such that <code>e1 == e2</code>.
+     * <p>
+     * The Stream will never contain <code>null</code> elements.
+     * <p>
+     * This is <em>an inexpensive O(1) operation</em> that will complete in
+     * constant time regardless of the number of entities in the underlying
+     * database.
+     * <p>
+     * The returned stream is aware of its own pipeline and will optionally
+     * <em>optimize its own pipeline</em> whenever it encounters a <em>Terminal
+     * Operation</em> so that it will only iterate over a minimum set of
+     * matching entities.
+     * <p>
+     * When a Terminal Operation is eventually called on the {@link Stream},
+     * that execution time of the Terminal Operation will depend on the
+     * optimized pipeline and the entities in the underlying database.
+     * <p>
+     * The Stream will be automatically
+     * {@link Stream#onClose(java.lang.Runnable) closed} after the Terminal
+     * Operation is completed or if an Exception is thrown during the Terminal
+     * Operation.
+     * <p>
+     * Some of the <em>Terminal Operations</em> are:
+     * <ul>
+     * <li>{@link Stream#forEach(java.util.function.Consumer) forEach(Consumer)}
+     * <li>{@link Stream#forEachOrdered(java.util.function.Consumer) forEachOrdered(Consumer)}
+     * <li>{@link Stream#toArray() toArray()}
+     * <li>{@link Stream#toArray(java.util.function.IntFunction) toArray(IntFunction)}
+     * <li>{@link Stream#reduce(java.util.function.BinaryOperator) reduce(BinaryOperation}
+     * <li>{@link Stream#reduce(java.lang.Object, java.util.function.BinaryOperator) reduce(Object, BinaryOperator)}
+     * <li>{@link Stream#reduce(java.lang.Object, java.util.function.BiFunction, java.util.function.BinaryOperator) reduce(Object, BiFunction, BinaryOperator)}
+     * <li>{@link Stream#collect(java.util.stream.Collector) collect(Collector)}
+     * <li>{@link Stream#collect(java.util.function.Supplier, java.util.function.BiConsumer, java.util.function.BiConsumer) collect(Supplier, BiConsumer, BiConsumer)}
+     * <li>{@link Stream#min(java.util.Comparator) min(Comparator)}
+     * <li>{@link Stream#max(java.util.Comparator) min(Comparator)}
+     * <li>{@link Stream#count() count()}
+     * <li>{@link Stream#anyMatch(java.util.function.Predicate) anyMatch(Predicate)}
+     * <li>{@link Stream#noneMatch(java.util.function.Predicate) noneMatch(Predicate)}
+     * <li>{@link Stream#findFirst() findFirst()}
+     * <li>{@link Stream#findAny() findAny()}
+     * <li>{@link Stream#iterator() iterator()}
+     * </ul>
+     * <p>
+     * Any Terminating Operation may throw an Exception if the
+     * underlying database throws an Exception (e.g. an SqlException)
+     * <p>
+     * Because the Stream may short-circuit operations in the Stream pipeline,
+     * methods having side-effects (like
+     * {@link Stream#peek(java.util.function.Consumer) peek(Consumer)} will
+     * potentially be affected by the optimization.
+     * <p>
+     * Here are some examples of how the stream optimization might work:
+     * <ul>
+     * <li>
+     * <pre>{@code stream
+     *   .filter(Hare.NAME.equal("Henry")
+     *   .collect(toList());}</pre>
+     * <pre>{@code -> select * from hares where name='Henry'}</pre>
+     * </li>
+     * <li>
+     * <pre>{@code stream.count();}</pre>
+     * <pre>{@code -> select count(*) from hares}</pre>
+     * </li>
+     * <li>
+     * <pre>{@code stream
+     *   .filter(Hare.NAME.equal("Henry")
+     *   .count();}</pre>
+     * <pre>{@code -> select count(*) from hares where
+     *   name='Henry'}</pre>
+     * <p>
+     * </li>
+     * <li>
+     * <pre>{@code stream
+     *   .filter(Hare.NAME.equal("Henry")
+     *   .filter(Hare.AGE.greaterThan(5)
+     *   .count();}</pre>
+     * <pre>{@code -> select count(*) from hares where
+     *          name ='Henry'
+     *        and
+     *          age > 5}</pre>
+     * </li>
+     * </ul>
+     *
+     * @param <T> The element type (class token)
+     * @param entityClass a class token for an entity class (annotated with {@code @Entity})
+     * @return a new stream over all entities in this table in unspecified order
+     *
+     * @throws RuntimeException if an error occurs during a Terminal Operation
+     * (e.g. an SqlException is thrown by the underlying database)
+     *
+     * @see java.util.stream
+     * @see Stream
+     */
     <T> Stream<T> stream(Class<T> entityClass);
 
+    /**
+     * Closes this JPAStreamer and releases any resources potentially held.
+     * <p>
+     * If and only if this JPAStreamer was created using a {@code persistenceUnitName},
+     * the underlying EntityManagerFactory will be closed.
+     */
     void close();
 
+    /**
+     * Creates and returns a new JPAStreamerBuilder that will create a new
+     * {@link EntityManagerFactory} using the provided {@code persistenceUnitName}.
+     * <p>
+     *  Call the JPAStreamerBuilder::build method to create a new JPAStreamer instance.
+     * <p>
+     * The newly created EntityManagerFactory will be closed whenever a built
+     * JPAStreamer instance is closed.
+     *
+     * @param persistenceUnitName of the persistence unit as per the persistence.xml file
+     * @return a new JPAStreamerBuilder
+     */
     static JPAStreamerBuilder createJPAStreamerBuilder(final String persistenceUnitName) {
         return RootFactory
             .getOrThrow(JPAStreamerBuilderFactory.class, ServiceLoader::load)
             .create(persistenceUnitName);
     }
+
+    /**
+     * Creates and returns a new JPAStreamerBuilder that will use the provided
+     * {@code entityManagerFactory}.
+     * <p>
+     *  Call the JPAStreamerBuilder::build method to create a new JPAStreamer instance.
+     * <p>
+     * The provided {@code entityManagerFactory} will <em>not</em> be closed whenever a built
+     * JPAStreamer instance is closed.
+     *
+     * @param entityManagerFactory to be used by the JPAStreamer
+     * @return a new JPAStreamerBuilder
+     */
 
     static JPAStreamerBuilder createJPAStreamerBuilder(final EntityManagerFactory entityManagerFactory) {
         return RootFactory
