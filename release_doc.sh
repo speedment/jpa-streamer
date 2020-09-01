2677607a33e4f2435539e22ba1ace201d75b11b9
Per Minborg <minborg@speedment.com>
2020-09-01 11:20:03 +0200

    Add script to release docs

---
+++ release_doc.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# JPAstreamer - Express JPA queries with Java Streams
+# Copyright (c) 2020-2020, Speedment, Inc. All Rights Reserved.
+#
+# License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+#
+# This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Lesser General Public License for more details.
+#
+# See: https://github.com/speedment/jpa-streamer/blob/master/LICENSE
+#
+
+#Fail on any error
+set -e
+
+if [ $# -eq 0 ]
+  then
+    echo "Usage $0: version"
+    exit 1
+fi
+VERSION=$1
+
+echo "** Bumping to version $VERSION"
+
+echo "** Checking if we are on the 'master' branch"
+MASTER_BRANCH=`git branch | grep "* master" | wc -l`
+if [ "$MASTER_BRANCH" -ne 1 ]
+  then
+    echo "Not on the 'master' branch"
+    exit 1
+fi
+
+read -p "Press enter to continue"
+
+echo "** Building version $VERSION"
+cd docs
+antora --fetch site.yml
+
+echo "** Push changes to GitHub"
+cd ../../jpa-streamer-gh-pages/jpa-streamer
+git add --all
+git commit -m "Bump version to $VERSION"
+git push
+
+echo "Completed!"
+
