d47fb5791c03858cb78142af53997912a3c058d1
Julia Gustafsson <gustafsson.julia@live.se>
2020-09-08 16:30:18 +0200

    ChaUpdate docs to new API, add instructions on multiple sources

--- fieldgenerator/fieldgenerator-test/src/main/java/com/speedment/jpastreamer/fieldgenerator/test/Actor.java
+++ fieldgenerator/fieldgenerator-test/src/main/java/com/speedment/jpastreamer/fieldgenerator/test/Actor.java
@@ -12,11 +12,16 @@
  */
 package com.speedment.jpastreamer.fieldgenerator.test;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
 @Table(name = "actor", schema = "sakila")
 public class Actor {
 
@@ -31,40 +36,9 @@ public class Actor {
     @Column(name = "last_name", nullable = false, columnDefinition = "varchar(45)")
     private String lastName;
 
+    @Getter(value = AccessLevel.PRIVATE)
     @ManyToMany(mappedBy = "actors")
     private List<Film> films = new ArrayList<>();
-
-    public Integer getActorId() {
-        return actorId;
-    }
-
-    public void setActorId(Integer actorId) {
-        this.actorId = actorId;
-    }
-
-    public List<Film> getFilms() {
-        return films;
-    }
-
-    public void setFilms(List<Film> films) {
-        this.films = films;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     @Override
     public String toString() {
