c3bd2abf0904ff5dbfaf37cf04d76dcdeffd9ff2
Per Minborg <minborg@speedment.com>
2020-09-16 16:23:59 +0200

    Add module announcer

---
+++ announcer/src/main/java/com/speedment/jpastreamer/announcer/Announcer.java
@@ -0,0 +1,37 @@
+/*
+ * JPAstreamer - Express JPA queries with Java Streams
+ * Copyright (c) 2020-2020, Speedment, Inc. All Rights Reserved.
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * See: https://github.com/speedment/jpa-streamer/blob/master/LICENSE
+ */
+package com.speedment.jpastreamer.announcer;
+
+/**
+ * Providers may provide implementations of this interface to make
+ * greeting announcements upon start.
+ * <p>
+ * The method provided here <em>should not</em> have any side-effects.
+ * In particular, this interface is not intended to manage
+ * components' life-cycles.
+ *
+ * @author  Per Minborg
+ * @since   0.8.0
+ */
+public interface Announcer {
+
+    /**
+     * Returns a greeting that is to be output upon
+     * system start-up.
+     *
+     * @return a greeting that is to be output upon
+     *         system start-up
+     */
+    String greeting();
+
+}
